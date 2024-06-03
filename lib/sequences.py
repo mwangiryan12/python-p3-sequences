287243f61a66afbf8d9801ae1e307e54542dc9b0
mwangiryan12 <mwangiryan12@gmail.com>
2024-06-03 16:53:13 +0300

    initial commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    if length==1:
+        print([0])
+    elif length == 2:
+        print([0,1])
+    elif length>2:
+        lst = [0,1]
+     
+        for i in range(2,length):
+            third = lst[-1]+lst[-2]
+           
+            lst.append(third)
+           
+        print (lst)
+    else:
+        print([])
+
+print_fibonacci(5)
